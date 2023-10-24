edc937df17da36d19b9deeb6e58b194b662aeb01
sowrd <15159885771@163.com>
2023-10-24 18:02:09 +0800

    scala

---
+++ scalaTest/src/main/scala/chapter01/Hello.scala
@@ -0,0 +1,16 @@
+package chapter01
+
+/**
+ * Description： TODO
+ *
+ * @author: 段世超
+ * @aate: Created in 2023/10/24 18:00
+ */
+object Hello {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello")
+    System.out.println("Hello")
+  }
+
+}
